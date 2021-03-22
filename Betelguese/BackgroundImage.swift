c5a0d7d9d813fb19796307f956eaa5df5d48eaa0
23 Aaron <aarondo99@outlook.com>
2021-03-22 03:23:45 +0000

    Initial commit

---
+++ Betelguese/BackgroundImage.swift
@@ -0,0 +1,27 @@
+//
+//  BackgroundImage.swift
+//  Odysseyra1n
+//
+//  Created by Aaron KJ on 11/06/2020.
+//  Copyright © 2020 23 Aaron. All rights reserved.
+//
+
+import Foundation
+
+    
+//    extension UIView {
+//    func addBackground() {
+//        // screen width and height:
+//        let width = UIScreen.mainScreen().bounds.size.width
+//        let height = UIScreen.mainScreen().bounds.size.height
+//
+//        let imageViewBackground = UIImageView(frame: CGRectMake(0, 0, width, height))
+//        imageViewBackground.image = UIImage(named: "YOUR IMAGE NAME GOES HERE")
+//
+//        // you can change the content mode:
+//        imageViewBackground.contentMode = UIViewContentMode.ScaleAspectFill
+//
+//        self.addSubview(imageViewBackground)
+//        self.sendSubviewToBack(imageViewBackground)
+//    }}
+    
---
+++ Betelguese/WindowController.swift
@@ -0,0 +1,16 @@
+//
+//  WindowController.swift
+//  Odysseyra1n
+//
+//  Created by 23 Aaron on 12/06/2020.
+//  Copyright © 2020 23 Aaron. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+    
+    @IBOutlet weak var goTouchBarButton: NSButton!
+    @IBOutlet weak var progressTouchBarLabel: NSTextField!
+    
+}
